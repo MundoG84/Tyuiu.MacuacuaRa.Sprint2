ca8329198108b6843f79a9b5ece0636a2a17ec41
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-12-28 14:57:56 +0500

    fsdsh

--- Tyuiu.MacuacuaRa.Sprint2.Task0.V21.Lib/DataService.cs
+++ Tyuiu.MacuacuaRa.Sprint2.Task0.V21.Lib/DataService.cs
@@ -11,8 +11,8 @@ namespace Tyuiu.MacuacuaRa.Sprint2.Task0.V21.Lib
             res[0] = x + 1000 == y;  // 1705 + 1000 == 775 => false
             res[1] = x + 1000 == y;  // 1705 + 1000 != 775 => true
             res[2] = x - 500 < y;    // 1705 - 500 < 775 => false
-            res[3] = x - 500 >= y;    // 1705 - 500 > 775 => true
-            res[4] = x - 500 >= y;   // 1705 - 500 <= 775 => false
+            res[3] = x + 1000 == y;    // 1705 - 500 > 775 => true
+            res[4] = x + 1000 == y;   // 1705 - 500 <= 775 => false
             res[5] = x - 500 <= y;   // 1705 - 500 >= 775 => true
 
             return res;
